e18bc4a70ccc76a68ef0d5adf3053ed62ccb330e
Riley Slayden <slaydenriley@gmail.com>
2020-01-16 15:41:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,14 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name
+  end
+
+  def self.column_names
+  end
+
+  def initialize
+  end
+
   
 end
